b73dbf0c48433877e331cfe3157e58f03ace3ad6
Victoria Holub <victoria_holub@epam.com>
2023-04-25 20:50:26 +0300

    part-1: create react-typescript. additional functionality with typescript

---
+++ src/interfaces/basic-interfaces.ts
@@ -0,0 +1,38 @@
+let personOne: unknown;
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface WorkerOne extends Person {
+  profession: string;
+}
+
+type X = {
+  name: string;
+  age: number;
+};
+
+type Y = X & {
+  profession: string;
+};
+
+type W = Person & {
+  job: string;
+};
+
+interface Student extends X {
+  class: string;
+}
+const personTwo: Y = {
+  profession: "office manager",
+  name: "Tom",
+  age: 29,
+};
+
+const PersonThree: WorkerOne = {
+  profession: "accountant",
+  name: "Bob",
+  age: 29,
+};
